315228ce0e94460fa2dedf300a0ee393094895b6
Steven Rodriguez <strodrig@redhat.com>
2020-08-19 11:02:15 -0500

    Start project

---
+++ com.redhat.simplenetcore.services/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace com.redhat.simplenetcore.services.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        protected IConfiguration Config;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            this.Config = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+
+            return new string[] {
+                Environment.GetEnvironmentVariable("FIRST"),
+                Environment.GetEnvironmentVariable("SECOND"),
+                this.Config.GetSection("Config:value1").Value,
+                this.Config.GetSection("Config:value2").Value,
+                this.Config.GetSection("Config:value3").Value
+            };
+
+        }
+    }
+}
---
+++ com.redhat.simplenetcore.services/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace com.redhat.simplenetcore.services
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(c =>
+            {
+                c.AddJsonFile("configuration/appsettings.json", optional: true, reloadOnChange: true);
+            })
+            .UseStartup<Startup>();
+    }
+
+}
